ba4b73d6475d011347dfa8b32c29d7e850c39e5b
Илья <Danilasyrala@gmail.com>
2020-04-16 18:25:56 +0700

    Добавил тип Дек

--- Dop.lab.2.pas
+++ Dop.lab.2.pas
@@ -1,1 +1,9 @@
+﻿{Реализовать вычисление квадратного корня из большого числа}
 
+type // Обозначаем Дек
+  TItem = ^PItem;
+  PItem = record
+  data: integer;
+  next: PItem;
+  prev: PItem;
+  end;
